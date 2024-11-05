ee5a389409e5338faa6f08eec501c27444bf0fe9
gustavo_c0imbra <gustavocoimbra04@gmail.com>
2024-11-05 14:47:52 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnAgendas = document.getElementById("btnAgendas");
+const btnClientes = document.getElementById("btnClientes");
+const btnFuncionarios = document.getElementById("btnFuncionarios");
+const btnServicos = document.getElementById("btnServicos");
+
+btnAgendas.addEventListener("click", () => {
+    
+});
+
+btnClientes.addEventListener("click", () => {
+
+});
+
+btnFuncionarios.addEventListener("click", () => {
+
+});
+
+btnServicos.addEventListener("click", () => {
+
+});
